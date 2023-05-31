800a7a43bd9f6a780d4d373bd365ee09072df378
GabrielDate <g_gabrielvictor@hotmail.com>
2023-05-30 21:37:35 -0300

    extensao e css

--- estilo/style.js
+++ estilo/style.js
@@ -1,3 +1,4 @@
+//#region SCROLL
 const element = document.querySelector("#container_livros");
 
 element.addEventListener('wheel', (event) => {
@@ -8,8 +9,12 @@ element.addEventListener('wheel', (event) => {
     
   });
 });
+//#endregion SCROLL
+
+//#region MODO ESCURO
 
 function myFunction() {
   var element = document.body;
   element.classList.toggle("dark-mode");
 }
+//#endregion MODO ESCURO
